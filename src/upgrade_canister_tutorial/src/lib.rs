1389ead4ff17160e09e5f8bb5d1821607b70db9b
magj2006 <magj2006@gmail.com>
2022-07-28 16:19:32 +0800

    update v0.4 (#4)
    Co-authored-by: scarecrow <scarecrow@Labs.local>

--- src/upgrade_canister_tutorial/src/lib.rs
+++ src/upgrade_canister_tutorial/src/lib.rs
@@ -7,12 +7,18 @@ use candid::{
 use ic_cdk::{export::serde::Serialize, println};
 use ic_cdk_macros::{post_upgrade, pre_upgrade};
 
-const VERSION: &str = "0.2";
+const VERSION: &str = "0.4";
 
 #[derive(Debug, Default, Clone, CandidType, Serialize, Deserialize)]
 pub struct Student {
     name: String,
     age: u16,
+}
+
+impl Student {
+    fn new(name: String, age: u16) -> Self {
+        Self { name, age }
+    }
 }
 
 impl Student {
@@ -44,30 +50,34 @@ impl<'de> ArgumentDecoder<'de> for Student {
     }
 }
 
-pub struct MaybeStable(Student);
+pub struct MaybeStable(Vec<Student>);
 
 impl ArgumentEncoder for MaybeStable {
     fn encode(self, ser: &mut candid::ser::IDLBuilder) -> candid::Result<()> {
         println!("encode for MaybeStable {}", VERSION);
-        self.0.encode(ser)
+        self.0.into_iter().map(|s| s.encode(ser)).collect()
     }
 }
 
 impl<'de> ArgumentDecoder<'de> for MaybeStable {
     fn decode(de: &mut candid::de::IDLDeserialize<'de>) -> candid::Result<Self> {
         println!("decode for MaybeStable {}", VERSION);
-        let s = Student::decode(de)?;
-        Ok(MaybeStable(s))
+        let mut v = vec![];
+        while let Ok(s) = Student::decode(de) {
+            v.push(s);
+        }
+        Ok(MaybeStable(v))
     }
 }
 
 thread_local! {
     static STUDENT: RefCell<Student> = RefCell::new(Student::default());
+    static CLASS: RefCell<Vec<Student>> = RefCell::new(Vec::new());
 }
 
 #[ic_cdk_macros::query]
 fn greet() -> String {
-    STUDENT.with(|s| format!("Hello: {:?}", s))
+    CLASS.with(|cs| format!("Hello: {:?}", cs.borrow()))
 }
 
 #[ic_cdk_macros::update]
@@ -80,11 +90,17 @@ fn set_age(age: u16) {
     STUDENT.with(|s| s.borrow_mut().set_age(age))
 }
 
+#[ic_cdk_macros::update]
+fn new_student(name: String, age: u16) {
+    CLASS.with(|class| class.borrow_mut().push(Student::new(name, age)))
+}
+
 #[pre_upgrade]
 fn pre_upgrade() {
     println!("pre version {}", VERSION);
 
-    let ms = MaybeStable(STUDENT.with(|s| s.borrow().clone()));
+    // let ms = MaybeStable(STUDENT.with(|s| s.borrow().clone()));
+    let ms = MaybeStable(CLASS.with(|class| class.borrow().clone()));
     ic_cdk::storage::stable_save(ms).expect("stable save")
 }
 
@@ -93,5 +109,6 @@ fn post_upgrade() {
     println!("post version {}", VERSION);
 
     let ms = ic_cdk::storage::stable_restore::<MaybeStable>().expect("stable restore");
-    STUDENT.with(|s| *s.borrow_mut() = ms.0);
+    CLASS.with(|class| *class.borrow_mut() = ms.0);
+    // STUDENT.with(|s| *s.borrow_mut() = ms.0);
 }
